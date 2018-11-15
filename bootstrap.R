67959d3e6c2ade677dbe597f4416a1bee4e8e699
Arni Magnusson <arnima@hafro.is>
2018-11-15 09:44:13 +0100

    Shorten SHA-1, comment branch names, demo write.bib()

--- bootstrap.R
+++ bootstrap.R
@@ -1,24 +1,12 @@
 library(icesTAF)
 
 ## FLCore 2.6.6 [2018-02-21]
-taf.install("flr", "FLCore", "d0333c17e8ba093de41d426df88db4776fcdc8c7")
+taf.install("flr", "FLCore", "d0333c1")
 
-## stockassessment 0.5.4 [2017-10-11]
-taf.install("fishfollower", "SAM", "25b35914cdf2f210a8b3bc0712e32dfbc1dde73b",
-            subdir="stockassessment")
+## FLSAM 2.1.0, develop_V2 branch [2018-03-19]
+taf.install("flr", "FLSAM", "7e078fa")
 
-## FLSAM 2.1.0 [2018-01-24]
-taf.install("flr", "FLSAM", "7e078fa5258ce7051fe7db39716ca66fda84c750")
+## stockassessment 0.5.4, components branch [2018-03-12]
+taf.install("fishfollower", "SAM", "25b3591", subdir="stockassessment")
 
-################################################################################
-
-## Notes
-##
-## Option 1. API with warning
-## download("https://api.github.com/repos/flr/FLCore/tarball/d0333c17e8ba093de41d426df88db4776fcdc8c7", destfile="x1.tar.gz")
-##
-## Option 2. API and suppress warning
-## suppressWarnings(download("https://api.github.com/repos/flr/FLCore/tarball/d0333c17e8ba093de41d426df88db4776fcdc8c7", destfile="x2.tar.gz"))
-##
-## Option 3. CodeLoad server
-## download("https://codeload.github.com/flr/FLCore/legacy.tar.gz/d0333c17e8ba093de41d426df88db4776fcdc8c7", destfile="x3.tar.gz")
+## bibtex::write.bib(taf.library())

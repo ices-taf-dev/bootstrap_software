cdc68436fe65103e54447bd706599a5e057d1f45
Arni Magnusson <arnima@hafro.is>
2018-11-15 10:16:57 +0100

    Install packages in order of dependency

--- bootstrap.R
+++ bootstrap.R
@@ -1,15 +1,17 @@
 library(icesTAF)
 
+## FLCore and stockassessment are required by FLSAM, so install those first
+
 ## FLCore 2.6.6 [2018-02-21]
 taf.install("flr", "FLCore", "d0333c1")
-
-## FLSAM 2.1.0, develop_V2 branch [2018-03-19]
-taf.install("flr", "FLSAM", "7e078fa")
 
 ## stockassessment 0.5.4, components branch [2018-03-12]
 taf.install("fishfollower", "SAM", "25b3591", subdir="stockassessment")
 
-## bibtex::write.bib(taf.library())
+## FLSAM 2.1.0, develop_V2 branch [2018-03-19]
+taf.install("flr", "FLSAM", "7e078fa")
+
+## bibtex::write.bib(taf.library(), "bootstrap/DEPENDENCIES.bib")
 
 
 
